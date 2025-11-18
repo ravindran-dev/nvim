5a6845cd3cc6a752096dbab85c20b3445d8b1f86
ravindran-dev <ravindrans.dev@gmail.com>
2025-11-18 13:28:35 +0530

    fix(lsp): fixed bugs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,44 @@
--- plugins/lsp.lua
-local mason = require('mason')
-local mason_lsp = require('mason-lspconfig')
-local lspconfig = require('lspconfig')
-
+local mason = require("mason")
+local mason_lsp = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
 
 mason.setup()
-mason_lsp.setup({ automatic_installation = true })
 
+mason_lsp.setup({
+  ensure_installed = {
+    "pyright",
+    "clangd",
+    "tsserver",
+    "rust_analyzer",
+    "gopls",
+    "lua_ls",
+  },
+  automatic_installation = true,
+})
 
--- common on_attach
 local on_attach = function(client, bufnr)
-local bufmap = function(mode, lhs, rhs)
-vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
+  local bufmap = function(mode, lhs, rhs)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
+  end
+  bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 end
-bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-bufmap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-end
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- enhance capabilities for nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-local cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp') and require('cmp_nvim_lsp')
-if cmp_nvim_lsp then capabilities = cmp_nvim_lsp.default_capabilities(capabilities) end
+local servers = {
+  "pyright",
+  "clangd",
+  "tsserver",
+  "rust_analyzer",
+  "gopls",
+  "lua_ls",
+}
 
-
--- default servers to setup
-local servers = { 'pyright', 'clangd', 'tsserver', 'rust_analyzer', 'gopls', 'lua_ls' }
-for _, lsp in ipairs(servers) do
-lspconfig[lsp].setup({ on_attach = on_attach, capabilities = capabilities })
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
