fafe270b678a71aba6e8f86cd739196e6dd5d727
ravindran-dev <ravindrans.dev@gmail.com>
2025-12-04 10:49:50 +0530

    fix(keymaps): bug fix in clipboard

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,6 +16,12 @@ map('n', '<C-h>', '<C-w>h', opts)
 map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
+-- Copy selected text using Ctrl + C
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+-- Paste with Ctrl + V
+vim.keymap.set("i", "<C-v>", '<Esc>"+pa', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-v>", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "<C-v>", '"+p', { noremap = true, silent = true })
 
 
 map('n', '<leader>w', ':w<CR>', opts)
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,47 +1,33 @@
 local o = vim.opt
 
-
-
 o.mouse = "a"
 o.clipboard = "unnamedplus"
+
+-- Line numbers
 o.number = true
 o.relativenumber = true
+
+-- Colors / UI
 o.termguicolors = true
+o.signcolumn = "yes"
+o.cursorline = true
+o.wrap = false
 
-
+-- Indentation
 o.tabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
 
-
+-- Files
 o.backup = false
 o.writebackup = false
-
-
 o.swapfile = false
-
-
 o.undofile = true
 
-
-
-o.signcolumn = "yes"
-o.cursorline = true
-
-
-o.wrap = false
-
-
+-- Scrolling / performance
 o.scrolloff = 8
-
-
 o.sidescrolloff = 8
-
-
-
 o.updatetime = 250
 
-vim.opt.number = true
-
-
+-- Leader key
 vim.g.mapleader = " "
